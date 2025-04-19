bd569d55ab19ba7b77c8554186e58dd321074b0e
hanaoka2481 <hanaoka2481@gmail.com>
2025-04-19 15:16:28 +0900

    아아, Scene Transition하면 기존 AudioManager 싱글톤은 파괴되는구나..

--- Assets/Scripts/CafeScene/CafeSceneManager.cs
+++ Assets/Scripts/CafeScene/CafeSceneManager.cs
@@ -5,5 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class CafeSceneManager : MonoBehaviour
 {
-    
+    public void Start()
+    {
+        AudioManager.instance.PlayBgm(true, AudioManager.BGM.CoffeeCats); // BGM 재생
+    }
+
+    public void OnTestButtonClick()
+    {
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.ButtonSelect); // 버튼 클릭 사운드 재생
+        Debug.Log("Test Button Clicked"); // 테스트 버튼 클릭 시 로그 출력
+    }
 }
