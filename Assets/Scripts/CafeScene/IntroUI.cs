e225f7d10d04a711634a651cacce015f71bcc265
hanaoka2481 <hanaoka2481@gmail.com>
2025-04-19 17:54:50 +0900

    List<string> 을 차근차근 출력하도록 Intro 변경

--- Assets/Scripts/CafeScene/IntroUI.cs
+++ Assets/Scripts/CafeScene/IntroUI.cs
@@ -15,32 +15,42 @@ public class IntroUI : MonoBehaviour
     
     public void Open()
     {
-        string text = "이것은 테스트용 UI입니다. 하나둘삼넷오 여섯일곱여덟아홉열";
+        List<string> textList = new List<string>();
+        textList.Add("그날 인류는 떠올렸다.");
+        textList.Add("그들에게 지배당하던 공포를.");
+        textList.Add("새장 속에 갇혀 있던 굴욕을.");
+
+        string text = textList[0];
         gameObject.SetActive(true);
-        StartCoroutine(ShowText_Coroutine(text));
+        StartCoroutine(ShowText_Coroutine(textList));
     }
 
     // 글자가 시간에 따라 촤라락 바뀌도록 Coroutine으로 구현.
-    private IEnumerator ShowText_Coroutine(string text)
+    private IEnumerator ShowText_Coroutine(List<string> texts)
     {
         targetText.text = "";
-        string forwardText = "";
-        string backText = text;
-
-        Debug.Log("ShowText_Coroutine Start");
-
-        // 적당한 딜레이를 주면서 글자를 순차적으로 출력.
-        while(backText.Length != 0)
+        for(int i = 0; i < texts.Count; i++)
         {
-            forwardText += backText[0];
-            backText = backText.Remove(0, 1);
-            targetText.text = string.Format("<color=#FFFFFF>{0}</color><color=#000000>{1}</color>", forwardText, backText);
-            yield return new WaitForSeconds(0.1f);
+            string forwardText = "";
+            string backText = texts[i];
+
+            Debug.Log("ShowText_Coroutine Start");
+
+            // 적당한 딜레이를 주면서 글자를 순차적으로 출력.
+            while(backText.Length != 0)
+            {
+                forwardText += backText[0];
+                backText = backText.Remove(0, 1);
+                targetText.text = string.Format("<color=#FFFFFF>{0}</color><color=#000000>{1}</color>", forwardText, backText);
+                yield return new WaitForSeconds(0.1f);
+            }
+            
+            yield return new WaitForSeconds(1f); // 다음 문장으로 넘어가기 전 1초 대기.
         }
-        yield return new WaitForSeconds(10f); // 10초동안 결과를 보여줌.
-        Close();
+        // yield return new WaitForSeconds(10f); // 10초동안 결과를 보여줌.
         Debug.Log("ShowText_Coroutine End");
         // onTextCompleted.Invoke(); // 이벤트 Invoke 해도 사용할곳이 없어서 비활성화함.
+        Close();
     }
     public void Close()
     {
