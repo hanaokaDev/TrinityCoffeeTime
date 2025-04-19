b943eeb0f9ec93d6db13aa81850bb7d9a9cea8b2
hanaoka2481 <hanaoka2481@gmail.com>
2025-04-19 14:32:09 +0900

    Added Button Sound Effect

--- Assets/Scripts/MainMenuScene/MainMenuManager.cs
+++ Assets/Scripts/MainMenuScene/MainMenuManager.cs
@@ -5,21 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public Button startButton;
-    public Button exitButton;
+
+    public Button newGameButton;
+    public Button quitButton;
+    public Button settingButton;
+
 
     void Start()
     {
         AudioManager.instance.PlayBgm(true); // BGM 재생
     }
 
-    void OnNewGameButtonClicked()
+    private void PlayButtonClickSound()
     {
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.ButtonSelect); // 버튼 클릭 사운드 재생
+    }
+
+    public void OnNewGameButtonClicked()
+    {
+        PlayButtonClickSound();
         SceneManager.LoadScene("GameScene"); // 게임 씬으로 전환
     }
 
-    void OnExitButtonClicked()
+    public void OnQuitButtonClicked()
     {
+        PlayButtonClickSound();
         if (Application.isEditor) // 에디터에서 실행중일 때
         {
             UnityEditor.EditorApplication.isPlaying = false; // 에디터 종료
@@ -28,5 +38,12 @@ public class MainMenuManager : MonoBehaviour
         {
             Application.Quit(); // 애플리케이션 종료
         }
+    }
+
+    // TODO: 설정 버튼 클릭 시 동작 추가
+    public void OnSettingButtonClicked()
+    {
+        PlayButtonClickSound();
+        Debug.Log("Setting Button Clicked"); // 설정 버튼 클릭 시 로그 출력
     }
 }
