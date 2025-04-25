34bab49650748eb0bb7776d19eafd1308fce738f
hanaoka2481 <hanaoka2481@gmail.com>
2025-04-25 17:55:01 +0900

    정수기도 에스프레소 기계와 똑같이 구현함.

---
+++ Assets/Scripts/CafeScene/WaterPurifier.cs
@@ -0,0 +1,81 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterPurifier : MonoBehaviour
+{
+    [SerializeField]
+    public Sprite nextUseButtonSprite;
+    // private SpriteRenderer spriteRenderer; // TODO: Highlighting 효과
+
+    public enum State { IDLE, BUSY, DONE, DIRTY};
+    public State currentState;
+
+    public ProgressBar progressBar;
+
+    int timeLeft = 0;
+    public int brewTime; // 초 단위로 설정
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        currentState = State.IDLE;
+        progressBar.SetMaxValue(brewTime);
+        progressBar.SetValue(0);
+
+        // TODO: Highlighting 효과
+        // spriteRenderer = GetComponent<SpriteRenderer>();
+        // var inst = Instantiate(spriteRenderer.material);
+        // spriteRenderer.material = inst;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Water Machine Triggered");
+        var character = collision.GetComponent<PlayerMover>();
+        if(character == null) {
+            Debug.Log("Water Machine Triggered but character is null");
+        }
+
+        if(character != null && character.isOwned)
+        {
+            // spriteRenderer.material.SetFloat("_Highlighted", 1f); // TODO: Highlighting 효과
+            HudManager.Instance.SetUseButton(nextUseButtonSprite, OnClickWaterMachine);
+        }
+        else{
+            Debug.Log("Water Machine Triggered but not owned");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Water Machine Exitted");
+        var character = collision.GetComponent<PlayerMover>();
+        if(character != null && character.isOwned)
+        {
+            // spriteRenderer.material.SetFloat("_Highlighted", 0f); // TODO: Highlighting 효과
+            HudManager.Instance.UnsetUseButton();
+        }
+        else{
+            Debug.Log("Water Machine Exitted but not owned");
+        }
+    }
+    
+    public void OnClickWaterMachine()
+    {
+        Debug.Log("Water Machine Clicked");
+        float currentValue = progressBar.GetValue();
+        progressBar.SetValue(currentValue + 1);
+    }
+
+}
