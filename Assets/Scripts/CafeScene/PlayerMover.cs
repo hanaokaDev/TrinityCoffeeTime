7c6b96f32927ddf9920355b26a1eb0ff403b84ca
hanaoka2481 <hanaoka2481@gmail.com>
2025-04-25 10:57:48 +0900

    Added Collider2D for each objects

--- Assets/Scripts/CafeScene/PlayerMover.cs
+++ Assets/Scripts/CafeScene/PlayerMover.cs
@@ -47,31 +47,26 @@ public class PlayerMover : MonoBehaviour
         {
             keyPressed = true;
             moveDirection = MoveDirection.UP;
-            Debug.Log("Moving UP");
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             keyPressed = true;
             moveDirection = MoveDirection.DOWN;
-            Debug.Log("Moving DOWN");
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             keyPressed = true;
             moveDirection = MoveDirection.LEFT;
-            Debug.Log("Moving LEFT");
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             keyPressed = true;
             moveDirection = MoveDirection.RIGHT;
-            Debug.Log("Moving RIGHT");
         }
         else{
             // 키가 눌리지 않았을 때 IDLE 상태로 설정
             keyPressed = false;
             moveDirection = MoveDirection.IDLE;
-            Debug.Log("IDLE");
         }
         
         if(keyPressed){
